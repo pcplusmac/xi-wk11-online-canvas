ae3cfd65ff43cf27fbfc7bb74e786fa38a5f7533
el-sam <samurai.h@yahoo.com>
2025-11-12 13:10:55 +0800

    call, bind, and apply all mirrorred

--- context.js
+++ context.js
@@ -1,6 +1,3 @@
-function someFunc() {
-    console.log(this)
-}
 
 const objA = {
     name:"aa",
@@ -12,10 +9,21 @@ const objB = {
     age: 23
 }
 const objC = {
-    name:"cd",
+    name:"cc",
     age: 56
 }
 const objD = {
     name:"dd",
     age:48   
 }
+
+function someFunc(food, sport) {
+    console.log(`the object name is: <${this.name}>; \nthe age of this object is "${this.age}"; \nI love ${food};\tmy favourite sport is ${sport}`)
+}
+
+someFunc.call(objA,"KFC","SOCCER")
+someFunc.apply(objD,['Rice','basketball'])
+someFunc.bind(objB,"PIZZA","RUGBY")() // note: with the (), someFunc will not be invoked. 
+// you can alos save the bind to a variable to invoke later whenever you like. 
+const newFuncBind = someFunc.bind(objC,'Macas','waterball','blue')
+newFuncBind()
