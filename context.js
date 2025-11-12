869cee1f9f9799bc1a1964f5699ac66cabb891ce
el-sam <samurai.h@yahoo.com>
2025-11-12 12:08:39 +0800

    objects and function created, ready to mirroring the context content for w11

---
+++ context.js
@@ -0,0 +1,21 @@
+function someFunc() {
+    console.log(this)
+}
+
+const objA = {
+    name:"aa",
+    age: 18   
+}
+
+const objB = {
+    name:"bb",
+    age: 23
+}
+const objC = {
+    name:"cd",
+    age: 56
+}
+const objD = {
+    name:"dd",
+    age:48   
+}
